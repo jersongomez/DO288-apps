987f51793800b8147ded23c2d4f8a41a0969d562
Jerson Gomez <jermetrix@gmail.com>
2022-01-23 16:11:06 -0500

    Update index.php

--- quotes/index.php
+++ quotes/index.php
@@ -1,4 +1,9 @@
 <?php
     print "This app returns a random funny quote.\n"
-        print "$HELLO_AUD.\n"
+        if(isset($HELLO_AUD) && $HELLO_AUD != null) {
+            print "$HELLO_AUD Ingeniero.\n"
+        } else {
+            print "Hola Ingenieros.\n"
+        }
+        
 ?>
